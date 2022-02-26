805f40f4b120e68d0bc145cf6eedd25fd84d627e
mehediitdba~ <mehediitdba@gmail.com>
2022-02-26 23:05:41 +0600

    added

---
+++ program.py
@@ -0,0 +1,16 @@
+#input phase
+name = input("Input Your Name : ")
+
+#process phase
+name = name + "!"
+
+#output fhase->
+print(name)
+
+#input phase
+fname = input("Input your first name : ")
+lname = input("Input your last name : ")
+
+fulname=fname+" @ "+lname
+
+print(fulname)
